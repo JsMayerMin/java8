03dd574fd35b63cb1934fdbf5301e9cd23848797
JsMayerMin <js.mayer.min@gmail.com>
2017-10-06 20:47:00 +0900

    람다로 테스팅

---
+++ src/examples/lambdaTesting830/PointTest.java
@@ -0,0 +1,27 @@
+package examples.lambdaTesting830;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PointTest {
+
+    @Test
+    public void testMoveRightBy() throws Exception {
+        Point p1 = new Point(5,5);
+        Point p2 = p1.moveRightBy(5);
+
+        assertEquals(15, p2.getX());
+        assertEquals(5, p2.getY());
+    }
+
+    @Test
+    public void testComparingWithLambda() throws Exception {
+        Point p1 = new Point(10,15);
+        Point p2 = new Point(10,20);
+
+        int result = Point.compareByXAndThenY.compare(p1,p2);
+        assertEquals(-1, result);
+    }
+
+}
